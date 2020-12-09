c745e1c1824566070bab2f211a0baa5ecef40273
zulkafeel <60641354+zulkafeel@users.noreply.github.com>
2020-12-09 12:40:11 +0530

    Update TestRunnerwithTestng.java

--- src/test/java/testrunner_demo/TestRunnerwithTestng.java
+++ src/test/java/testrunner_demo/TestRunnerwithTestng.java
@@ -5,9 +5,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(plugin = {"json:target/cucumber.json"},
 features="C:\\salenium practice\\com.sprint2.demo\\target\\test-classes\\featurefiles",
-glue={"stepdefination_demo"},
-tags={"@unsuccessful_registration1"
-		}
+glue={"stepdefination_demo"}
+//tags={"@unsuccessful_registration1"}
+		
 )
 
 public class TestRunnerwithTestng extends AbstractTestNGCucumberTests{
